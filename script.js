0db8545f131c11f6c506bcee4aac3692a7362500
willnguyen0903 <trnguyen0903@gmail.com>
2023-12-20 17:29:17 -0500

    update main functions.
    -added color picker and eraser.
    -added size slider.

--- script.js
+++ script.js
@@ -1,62 +1,83 @@
 const gridContainer = document.createElement('div');
-gridContainer.setAttribute('class', 'container');
-document.body.appendChild(gridContainer);
+gridContainer.setAttribute('class', 'gridContainer');
+const container = document.querySelector('.container');
+container.appendChild(gridContainer);
 
-// Create a 16x16 div element
-const rows = 100;
-const columns = 100;
-const gridWidth = 100 / rows;
-for (let i = 0; i < rows; i++) {
-  for (let j = 0; j < columns; j++) {
-    const grid = document.createElement('div');
-    grid.setAttribute('id', 'grids');
-    grid.style.width = `${gridWidth}%`;
-    grid.style.height = `${gridWidth}%`;
-    gridContainer.appendChild(grid);
+const slider = document.getElementById("myRange");
+const output = document.getElementById("demo");
+
+// Initial grid creation
+createGrid(slider.value, slider.value);
+
+function createGrid(rows, columns) {
+  const gridWidth = 100 / rows;
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const grid = document.createElement('div');
+      grid.setAttribute('class', 'grid-item'); // Add a class for styling
+      grid.style.width = `${gridWidth}%`;
+      grid.style.height = `${gridWidth}%`;
+      gridContainer.appendChild(grid);
+    }
   }
-}
-//coloring function: hold lclick and drag to color
-let isMouseDown = false; 
-const grids = document.querySelectorAll('#grids');
-    grids.forEach((gridItem) => {
-      gridItem.addEventListener('mousedown', function () {
-        isMouseDown = true;
-        coloring(gridItem);
-      });
 
-      gridItem.addEventListener('mouseup', function() {
-        isMouseDown = false;
-      });
+  // Set up event listeners after creating the grid
+  const grids = document.querySelectorAll('.grid-item');
+  let isMouseDown = false;
 
-      gridItem.addEventListener('mouseenter', function() {
-        if (isMouseDown) {
-          coloring(gridItem);
-        }
-      });
-    });
-    
-
-function coloring(element) {
-  element.style.backgroundColor = 'lightcoral';
-}
-
-//reset button: reset the board on click
-const resetButton = document.querySelector('#reset-btn');
-resetButton.addEventListener('click',clearBoard);
-
-function clearBoard() {
   grids.forEach((gridItem) => {
-    gridItem.style.backgroundColor = 'white'
+    gridItem.addEventListener('mousedown', function (event) {
+      isMouseDown = true;
+      event.preventDefault();
+      coloring(gridItem);
+    });
+
+    gridItem.addEventListener('mouseup', function() {
+      isMouseDown = false;
+    });
+
+    gridItem.addEventListener('mousemove', function() {
+      if (isMouseDown) {
+        event.preventDefault();
+        coloring(gridItem);
+      }
+    });
   });
 }
 
-//resize button 
-const sizeSelector = document.querySelector('#size');
-resizeButton.addEventListener('click',resize);
+// Size slider
+output.innerHTML = slider.value;
 
-function resize() {
-  
+slider.addEventListener("input", function() {
+  output.innerHTML = this.value;
+
+  // Clear existing grids
+  gridContainer.innerHTML = '';
+
+  // Create a new grid based on the updated slider value
+  createGrid(this.value, this.value);
+});
+
+// Coloring function: hold left-click and drag to color
+const colorPicker = document.querySelector('#colorPicker');
+function coloring(element) {
+  element.style.backgroundColor = colorPicker.value;
 }
 
+// Reset button & eraser
+const resetButton = document.querySelector('#reset-btn');
+resetButton.addEventListener('click', clearBoard);
 
+function clearBoard() {
+  const grids = document.querySelectorAll('.grid-item');
+  grids.forEach((gridItem) => {
+    gridItem.style.backgroundColor = 'white';
+  });
+}
+
+const eraser = document.querySelector('#eraser');
+eraser.addEventListener('click', () => {
+  colorPicker.value = '#FFFFFF';
+});
 
